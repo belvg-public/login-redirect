e58926cc60baccbe7f8c6c42531bd5defe49ca84
A. A. Treitjak <a.treitjak@gmail.com>
2013-04-08 16:44:09 +0300

    Check the user's group and do the redirection.

--- app/code/local/Belvg/Loginredirect/Model/Observer/Customer.php
+++ app/code/local/Belvg/Loginredirect/Model/Observer/Customer.php
@@ -10,8 +10,27 @@ class Belvg_Loginredirect_Model_Observer_Customer
     {
         if (Mage::helper('loginredirect')->isEnabled()) {
             $_session = $this->_getSession();
-            $_session->setBeforeAuthUrl(Mage::helper('loginredirect')->getRedirectUrl());
+
+            /* Check the user's group and do the redirection.*/
+            if($this->_checkActionForCustomerGroup()) {
+                $_session->setBeforeAuthUrl(Mage::helper('loginredirect')->getRedirectUrl());
+            }
         }
+    }
+
+
+    protected function _checkActionForCustomerGroup()
+    {
+        /* @var $customer Mage_Customer_Model_Customer */
+        $customer = $this->_getSession()->getCustomer();
+
+        if($customer) {
+            if($customer->getGroupId() == Mage::helper('loginredirect')->getConfigValue('group')) {
+                return TRUE;
+            }
+        }
+
+        return FALSE;
     }
 
     /**
